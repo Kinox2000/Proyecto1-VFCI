fb3c58525b55e3e8a9de52066bed26858cd2c933
valfarodd <101215264+valfarodd@users.noreply.github.com>
2022-09-11 14:23:22 -0600

    Update test.sv
    se agrega el ambiente

--- test.sv
+++ test.sv
@@ -1,43 +1,46 @@
 //test
-'timescale 1ns/1ps
-'include "library.sv"//archivo con el dut
-'include "Driver.sv"
-'include "agente_generador.sv"
-'include "transacciones.sv"
-'include "interface_DUT.sv"
 
-module test;
-  parameter WIDTH =16;
-  parameter DISPOSITIVOS = 2;
-  parameter MAX_RETARDO=10;
+class test#(parameter WIDTH=16, parameter DISPOSITIVOS =2,parameter MAX_RETARDO=10  );
+
   
   tipo_trans trans_elegida; //se define el tipo de transaccion que quiero
   tipo_reporte rep_elegido;// se define el tipo de reporte que quiero
   
   Comando_Test_Agente_mbx Test_Agente_mbx; //creo el mailbox del test al agente
   Comando_Test_Scoreboard_mbx Test_Scoreboard_mbx;
-  //definir ambiente de prueba.
   
-  //
+  //definir ambiente de prueba.
+  ambiente #(.WIDTH(WIDTH),.MAX_RETARDO(MAX_RETARDO),.DISPOSITIVOS(DISPOSITIVOS)) ambiente_inst;
+  
+  
   function new;
   //instanciacion de los mailboxes
-  Test_Agente_mbx =new();
+    Test_Agente_mbx =new();
+    Test_Scoreboard_mbx =new();
+    
+    
+    //definicion ambiente
+    ambiente_inst=new();
+    ambiente_inst.agente_generador_inst.Test_Agente_mbx=Test_Agente_mbx;
   endfunction
   
-  initial begin
-  $display("Tiempo %0t Test: Test fue inicializado", $time);
+  task run;
+    $display("Tiempo %0t Test: Test fue inicializado", $time);
+    fork
+      ambiente_inst.run();
+    join_none
+    
+    trans_elegida = Trans_broadcast;
+    Test_Agente_mbx.put(trans_elegida);
+    $display("Tiempo %0t Test: Se envia la primera instruccion al agente_generador-> trans_elegida= %s",$time, trans_elegida);
 
-  trans_elegida = Trans_broadcast;
-  Test_Agente_mbx.put(trans_elegida);
-  $display("[Tiempo %0t Test: Se envia la primera instruccion al agente_generador-> trans_elegida= %s",$time, trans_elegida);
+    rep_elegido =Reporte_completo;
+    Test_Scoreboard_mbx.put(rep_elegido);
+    $display("Tiempo %0t Test: Se envia la segunda instruccion al agente_scoreboard-> rep_elegido= %s",$time, rep_elegido);
 
-  rep_elegido =Reporte_completo;
-  Test_Scoreboard_mbx.put(rep_elegido);
-  $display("[Tiempo %0t Test: Se envia la segunda instruccion al agente_scoreboard-> rep_elegido= %s",$time, rep_elegido);
+    #1500000
 
-  #1500000
+    $finish;
 
-  $finish;
-
-  end
-endmodule
+  endtask
+endclass
